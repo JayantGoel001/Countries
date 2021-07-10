b1d3c50236a28ba99949580a23617e0a22380c20
JayantGoel001 <jgoel92@gmail.com>
2021-07-10 05:45:56 +0530

    Initial Commit

--- mvc/controllers/countries.js
+++ mvc/controllers/countries.js
@@ -1,11 +1,41 @@
 const request = require("request");
 
-
+let domainPath = "http://localhost:3000";
+const requestOpt = (path,method)=>{
+    return {
+        url : `${domainPath}${path}`,
+        method : method
+    }
+}
 let getCountryList = (req,res)=>{
-    res.render('index',{ title : "RESTFull Routing" });
+    const path = "/api/countries/";
+    const requestOption = requestOpt(path,'GET');
+    request(requestOption,(err,response)=>{
+        if(err){
+            return res.json({ error : err });
+        }
+        let countries = JSON.parse(response.body).countries;
+        if (response.statusCode=== 200){
+            return res.render('countries',{ title : "List Of Countries",countries : countries });
+        }
+        res.json({ message : "Something Went Wrong" });
+    });
 }
 let getCountry = (req,res)=>{
-    res.render('index',{ title : "RESTFull Routing" });
+    const path = `/api/countries/${req.params.id}`;
+    const requestOption = requestOpt(path,'GET');
+
+    request(requestOption,(err,response)=>{
+        if(err){
+            return res.json({ error : err });
+        }
+        let country = JSON.parse(response.body).country;
+        if (response.statusCode=== 200){
+            return res.render('country',{ title : "Country",country : country });
+        }
+        res.json({ message : "Something Went Wrong" });
+    });
+
 }
 let createCountry = (req,res)=>{
     res.render('index',{ title : "RESTFull Routing" });
